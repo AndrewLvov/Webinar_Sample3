d172f76c2b94d2e3f1bf5a855c7f33b3a41b6370
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-01-11 08:55:34 +0200

    Added main.h

--- Webinar_Sample3/main.cpp
+++ Webinar_Sample3/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#include "main.h"
 
 using namespace std;
 
+void f() {
+	cout << "Hi!";
+}
+
 int main() {
-	cout << "Hello, World!\n";
+	ifstream ifs("in.txt");
+	string line;
+	getline(ifs, line);
+	cout << line;
 
 	return 0;
 }
